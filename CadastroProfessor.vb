051e3ad96d4ba92cfbd3c3ebbf62714998506e62
Luiz Palma <luizpalma16@gmail.com>
2025-04-30 12:03:50 -0300

    Verificação da unicidade do email no cadastro de professores.
    Controle da relação 1xn entre e-mail e professor no banco de dados via camada controller.

--- CadastroProfessor.vb
+++ CadastroProfessor.vb
@@ -2,10 +2,39 @@
 
 Public Class CadastroProfessor
     Private Sub btn_cadastrar_Click(sender As Object, e As EventArgs) Handles btn_cadastrar.Click
+
+        'TODO:
+        '   Verificar se os campos estão preenchidos
+        '       se não, exibir mensagem de erro (não esquecer o exit sub)
+        '   
+        '   Criar o chkbox de esconde/mostrar senha
+        '   Criar o botão de limpar (opcional)
+        '   Criar a confirmação da senha
+        '   crias os campos de endereço (vide o banco de dados)
+        '   crias os campos de telefone (vide o banco de dados)
+        '   criar a lógica de cadastrar novo professor
+        '       se sim, continua no form
+        '       se não, fecha o form e volta para inicial do adm
+
+
+
+
         Dim conexao As New SQLiteConnection(connectionString)
 
         Try
             conexao.Open()
+
+            Dim sqlVerificar As String = "SELECT COUNT(*) FROM tb_professores WHERE email = @email"
+            Using comandoVerificar As New SQLiteCommand(sqlVerificar, conexao)
+                comandoVerificar.Parameters.AddWithValue("@email", Txt_email.Text)
+                Dim qtdEmails As Integer = Convert.ToInt32(comandoVerificar.ExecuteScalar())
+                If (qtdEmails > 0) Then
+                    MsgBox("Email já cadastrado!", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Email já cadastrado")
+                    Txt_email.Focus()
+                    Exit Sub
+                End If
+            End Using
+
             Dim salt As Byte() = GerarSalt(10)
             Dim senha_hash = GerarHashSenha(Txt_senha.Text, salt)
 
@@ -34,6 +63,7 @@ Public Class CadastroProfessor
                 Txt_email.Text = ""
                 Txt_senha.Text = ""
                 Txt_nome.Focus()
+
 
                 MsgBox($"Professor {Txt_nome.Text} cadastrado com sucesso!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Cadastrado com sucesso")
 
