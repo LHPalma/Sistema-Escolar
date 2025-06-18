87b64d2e8e5fa6a757c3f7afd3023cfca1eec566
patrick-0990 <patrickwallenstein0990@gmail.com>
2025-06-18 16:46:44 -0300

    Criação do Frm_cadastrarNotas
    - Permite que o professor enviar e editar notas de alunos
    - Permite que o professor possa consultar as notas dos alunos

---
+++ Cadastro/Frm_cadastroNotas.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_cadastroNotas
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbAvaliacao = New System.Windows.Forms.ComboBox()
+        Me.cmbAluno = New System.Windows.Forms.ComboBox()
+        Me.nudNota = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCadastrar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.dgvNotas = New System.Windows.Forms.DataGridView()
+        Me.cmbAvaliacaoConsulta = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.nudNota, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvNotas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmbAvaliacao
+        '
+        Me.cmbAvaliacao.FormattingEnabled = True
+        Me.cmbAvaliacao.Location = New System.Drawing.Point(51, 123)
+        Me.cmbAvaliacao.Name = "cmbAvaliacao"
+        Me.cmbAvaliacao.Size = New System.Drawing.Size(242, 21)
+        Me.cmbAvaliacao.TabIndex = 0
+        '
+        'cmbAluno
+        '
+        Me.cmbAluno.FormattingEnabled = True
+        Me.cmbAluno.Location = New System.Drawing.Point(51, 220)
+        Me.cmbAluno.Name = "cmbAluno"
+        Me.cmbAluno.Size = New System.Drawing.Size(242, 21)
+        Me.cmbAluno.TabIndex = 1
+        '
+        'nudNota
+        '
+        Me.nudNota.Location = New System.Drawing.Point(51, 309)
+        Me.nudNota.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nudNota.Name = "nudNota"
+        Me.nudNota.Size = New System.Drawing.Size(226, 20)
+        Me.nudNota.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 105)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Avaliação"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 204)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Aluno"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(48, 293)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Nota"
+        '
+        'btnCadastrar
+        '
+        Me.btnCadastrar.Location = New System.Drawing.Point(57, 368)
+        Me.btnCadastrar.Name = "btnCadastrar"
+        Me.btnCadastrar.Size = New System.Drawing.Size(277, 59)
+        Me.btnCadastrar.TabIndex = 6
+        Me.btnCadastrar.Text = "CADASTRAR NOTA"
+        Me.btnCadastrar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(376, 368)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(277, 59)
+        Me.btnEditar.TabIndex = 7
+        Me.btnEditar.Text = "EDITAR NOTA"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'dgvNotas
+        '
+        Me.dgvNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvNotas.Location = New System.Drawing.Point(387, 12)
+        Me.dgvNotas.Name = "dgvNotas"
+        Me.dgvNotas.Size = New System.Drawing.Size(370, 294)
+        Me.dgvNotas.TabIndex = 8
+        '
+        'cmbAvaliacaoConsulta
+        '
+        Me.cmbAvaliacaoConsulta.FormattingEnabled = True
+        Me.cmbAvaliacaoConsulta.Location = New System.Drawing.Point(515, 312)
+        Me.cmbAvaliacaoConsulta.Name = "cmbAvaliacaoConsulta"
+        Me.cmbAvaliacaoConsulta.Size = New System.Drawing.Size(242, 21)
+        Me.cmbAvaliacaoConsulta.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(413, 320)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Filtro por avaliação"
+        '
+        'Frm_cadastroNotas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cmbAvaliacaoConsulta)
+        Me.Controls.Add(Me.dgvNotas)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnCadastrar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.nudNota)
+        Me.Controls.Add(Me.cmbAluno)
+        Me.Controls.Add(Me.cmbAvaliacao)
+        Me.Name = "Frm_cadastroNotas"
+        Me.Text = "Frm_cadastroNotas"
+        CType(Me.nudNota, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvNotas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbAvaliacao As ComboBox
+    Friend WithEvents cmbAluno As ComboBox
+    Friend WithEvents nudNota As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCadastrar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents dgvNotas As DataGridView
+    Friend WithEvents cmbAvaliacaoConsulta As ComboBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Cadastro/Frm_cadastroNotas.vb
@@ -0,0 +1,311 @@
+﻿Imports System.Data.SQLite
+
+Public Class Frm_cadastroNotas
+    Private Function NotaExiste(idAvaliacao As Integer, idAluno As Integer, conexao As SQLiteConnection) As Boolean
+        Dim sql As String = "SELECT COUNT(*) FROM tb_notas WHERE fk_id_avaliacao = @avaliacao AND fk_id_aluno = @aluno"
+        Using cmd As New SQLiteCommand(sql, conexao)
+            cmd.Parameters.AddWithValue("@avaliacao", idAvaliacao)
+            cmd.Parameters.AddWithValue("@aluno", idAluno)
+            Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+            Return count > 0
+        End Using
+    End Function
+
+    Private Sub btnCadastrar_Click(sender As Object, e As EventArgs) Handles btnCadastrar.Click
+
+        If cmbAvaliacao.SelectedIndex = -1 Then
+            MessageBox.Show("Selecione uma avaliação.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        If cmbAluno.SelectedIndex = -1 Then
+            MessageBox.Show("Selecione um aluno.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim idAvaliacao As Integer = Convert.ToInt32(cmbAvaliacao.SelectedValue)
+        Dim idAluno As Integer = Convert.ToInt32(cmbAluno.SelectedValue)
+        Dim nota As Decimal = nudNota.Value
+
+        Using conexao As New SQLiteConnection(connectionString)
+            conexao.Open()
+
+            If NotaExiste(idAvaliacao, idAluno, conexao) Then
+                MessageBox.Show("Já existe uma nota cadastrada para este aluno nesta avaliação.", "Nota existente", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Exit Sub
+            End If
+
+            Dim sql As String = "INSERT INTO tb_notas (fk_id_avaliacao, fk_id_aluno, nota, data_cadastro) 
+                             VALUES (@avaliacao, @aluno, @nota, DATETIME('now', 'localtime'))"
+
+            Using cmd As New SQLiteCommand(sql, conexao)
+                cmd.Parameters.AddWithValue("@avaliacao", idAvaliacao)
+                cmd.Parameters.AddWithValue("@aluno", idAluno)
+                cmd.Parameters.AddWithValue("@nota", nota)
+
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Nota cadastrada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                LimparCampos()
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        'Validações
+        If cmbAvaliacao.SelectedIndex = -1 Then
+            MessageBox.Show("Selecione uma avaliação.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        If cmbAluno.SelectedIndex = -1 Then
+            MessageBox.Show("Selecione um aluno.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim idAvaliacao As Integer = Convert.ToInt32(cmbAvaliacao.SelectedValue)
+        Dim idAluno As Integer = Convert.ToInt32(cmbAluno.SelectedValue)
+        Dim novaNota As Decimal = nudNota.Value
+
+        Using conexao As New SQLiteConnection(connectionString)
+            conexao.Open()
+
+            If Not NotaExiste(idAvaliacao, idAluno, conexao) Then
+                MessageBox.Show("Não existe uma nota cadastrada para este aluno nesta avaliação.", "Nota não encontrada", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Exit Sub
+            End If
+
+            Dim sql As String = "UPDATE tb_notas 
+                             SET nota = @nota, data_cadastro = DATETIME('now', 'localtime')
+                             WHERE fk_id_avaliacao = @avaliacao AND fk_id_aluno = @aluno"
+
+            Using cmd As New SQLiteCommand(sql, conexao)
+                cmd.Parameters.AddWithValue("@nota", novaNota)
+                cmd.Parameters.AddWithValue("@avaliacao", idAvaliacao)
+                cmd.Parameters.AddWithValue("@aluno", idAluno)
+
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Nota atualizada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                LimparCampos()
+            End Using
+        End Using
+    End Sub
+
+    Private Sub LimparCampos()
+        cmbAvaliacao.SelectedIndex = -1
+        cmbAluno.SelectedIndex = -1
+        nudNota.Value = 0
+    End Sub
+
+    Private Sub Frm_cadastroNotas_Load(sender As Object, e As EventArgs) Handles Me.Load
+        CarregarAvaliacoes()
+        CarregarAvaliacoesConsulta()
+        EstilizarDataGrid()
+    End Sub
+
+    Private Sub CarregarAvaliacoes()
+
+        Dim sql As String = "SELECT id_avaliacao, 
+                                 nome_professor || ' - ' || nome_disciplina || ' - ' || nome_turma || ' - ' || tipo || ' - ' || data_avaliacao AS descricao
+                          FROM vw_avaliacoes"
+
+        Using conexao As New SQLiteConnection(connectionString)
+            Using cmd As New SQLiteCommand(sql, conexao)
+                Dim dt As New DataTable()
+
+                Try
+                    conexao.Open()
+                    Dim da As New SQLiteDataAdapter(cmd)
+                    da.Fill(dt)
+
+                    cmbAvaliacao.DataSource = dt
+                    cmbAvaliacao.DisplayMember = "descricao"
+                    cmbAvaliacao.ValueMember = "id_avaliacao"
+                    cmbAvaliacao.SelectedIndex = -1
+
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao carregar avaliações: " & ex.Message)
+                End Try
+
+            End Using
+        End Using
+
+    End Sub
+
+    Private Sub CarregarAlunosDaAvaliacao(idAvaliacao As Integer)
+
+
+        Dim idTurma As Integer = -1
+
+        Dim sqlBuscaTurma As String = "SELECT pdt.fk_id_turma 
+                                    FROM tb_avaliacoes a
+                                    JOIN tb_professores_disciplinas_turmas pdt ON a.fk_id_prof_disc_turma = pdt.id_prof_disc_turma
+                                    WHERE a.id_avaliacao = @id_avaliacao"
+
+        Using conexao As New SQLiteConnection(connectionString)
+            conexao.Open()
+
+            Using cmdBusca As New SQLiteCommand(sqlBuscaTurma, conexao)
+                cmdBusca.Parameters.AddWithValue("@id_avaliacao", idAvaliacao)
+
+                Dim resultado = cmdBusca.ExecuteScalar()
+
+                If resultado IsNot Nothing Then
+                    idTurma = Convert.ToInt32(resultado)
+                Else
+                    MessageBox.Show("Turma não encontrada para esta avaliação.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Exit Sub
+                End If
+            End Using
+
+
+            Dim sqlAlunos As String = "SELECT a.id_aluno, a.nome 
+                                   FROM tb_alunos a
+                                   JOIN tb_alunos_turmas ta ON a.id_aluno = ta.fk_id_aluno
+                                   WHERE ta.fk_id_turma = @id_turma"
+
+            Using cmdAlunos As New SQLiteCommand(sqlAlunos, conexao)
+                cmdAlunos.Parameters.AddWithValue("@id_turma", idTurma)
+
+                Dim dt As New DataTable()
+                Dim da As New SQLiteDataAdapter(cmdAlunos)
+                da.Fill(dt)
+
+                cmbAluno.DataSource = dt
+                cmbAluno.DisplayMember = "nome"
+                cmbAluno.ValueMember = "id_aluno"
+                cmbAluno.SelectedIndex = -1
+            End Using
+
+        End Using
+
+    End Sub
+
+    Private Sub cmbAvaliacao_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles cmbAvaliacao.SelectionChangeCommitted
+        If cmbAvaliacao.SelectedIndex <> -1 Then
+            Dim idAvaliacao As Integer = Convert.ToInt32(cmbAvaliacao.SelectedValue)
+            CarregarAlunosDaAvaliacao(idAvaliacao)
+        End If
+    End Sub
+
+    Private Sub CarregarNota(idAvaliacao As Integer, idAluno As Integer)
+
+        Dim sql As String = "SELECT nota FROM tb_notas 
+                          WHERE fk_id_avaliacao = @avaliacao AND fk_id_aluno = @aluno"
+
+        Using conexao As New SQLiteConnection(connectionString)
+            Using cmd As New SQLiteCommand(sql, conexao)
+                cmd.Parameters.AddWithValue("@avaliacao", idAvaliacao)
+                cmd.Parameters.AddWithValue("@aluno", idAluno)
+
+                Try
+                    conexao.Open()
+                    Dim resultado = cmd.ExecuteScalar()
+
+                    If resultado IsNot Nothing Then
+                        nudNota.Value = Convert.ToDecimal(resultado)
+                    Else
+                        nudNota.Value = 0
+                    End If
+
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao buscar nota: " & ex.Message)
+                End Try
+
+            End Using
+        End Using
+
+    End Sub
+
+    Private Sub cmbAluno_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles cmbAluno.SelectionChangeCommitted
+        If cmbAvaliacao.SelectedIndex <> -1 And cmbAluno.SelectedIndex <> -1 Then
+            Dim idAvaliacao As Integer = Convert.ToInt32(cmbAvaliacao.SelectedValue)
+            Dim idAluno As Integer = Convert.ToInt32(cmbAluno.SelectedValue)
+            CarregarNota(idAvaliacao, idAluno)
+        End If
+    End Sub
+
+    Private Sub CarregarNotasPorAvaliacao(idAvaliacao As Integer)
+
+        Dim sql As String = "SELECT a.nome AS aluno, n.nota, av.tipo, av.nome AS avaliacao
+                          FROM tb_notas n
+                          JOIN tb_alunos a ON n.fk_id_aluno = a.id_aluno
+                          JOIN tb_avaliacoes av ON n.fk_id_avaliacao = av.id_avaliacao
+                          WHERE av.id_avaliacao = @avaliacao"
+
+        Using conexao As New SQLiteConnection(connectionString)
+            Using cmd As New SQLiteCommand(sql, conexao)
+                cmd.Parameters.AddWithValue("@avaliacao", idAvaliacao)
+
+                Dim dt As New DataTable()
+
+                Try
+                    conexao.Open()
+                    Dim da As New SQLiteDataAdapter(cmd)
+                    da.Fill(dt)
+
+                    dgvNotas.DataSource = dt
+
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao buscar notas: " & ex.Message)
+                End Try
+
+            End Using
+        End Using
+
+    End Sub
+
+    Private Sub cmbAvaliacaoConsulta_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles cmbAvaliacaoConsulta.SelectionChangeCommitted
+        If cmbAvaliacaoConsulta.SelectedIndex <> -1 Then
+            Dim idAvaliacao As Integer = Convert.ToInt32(cmbAvaliacaoConsulta.SelectedValue)
+            CarregarNotasPorAvaliacao(idAvaliacao)
+        End If
+    End Sub
+    Private Sub EstilizarDataGrid()
+
+        With dgvNotas
+            .ReadOnly = True
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .MultiSelect = False
+            .AllowUserToAddRows = False
+            .AllowUserToDeleteRows = False
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            .RowHeadersVisible = False
+            .DefaultCellStyle.BackColor = Color.White
+            .DefaultCellStyle.ForeColor = Color.Black
+            .DefaultCellStyle.SelectionBackColor = Color.DodgerBlue
+            .DefaultCellStyle.SelectionForeColor = Color.White
+        End With
+
+    End Sub
+
+    Private Sub CarregarAvaliacoesConsulta()
+
+        Dim sql As String = "SELECT id_avaliacao, 
+                                 nome_professor || ' - ' || nome_disciplina || ' - ' || nome_turma || ' - ' || tipo || ' - ' || data_avaliacao AS descricao
+                          FROM vw_avaliacoes"
+
+        Using conexao As New SQLiteConnection(connectionString)
+            Using cmd As New SQLiteCommand(sql, conexao)
+                Dim dt As New DataTable()
+
+                Try
+                    conexao.Open()
+                    Dim da As New SQLiteDataAdapter(cmd)
+                    da.Fill(dt)
+
+                    cmbAvaliacaoConsulta.DataSource = dt
+                    cmbAvaliacaoConsulta.DisplayMember = "descricao"
+                    cmbAvaliacaoConsulta.ValueMember = "id_avaliacao"
+                    cmbAvaliacaoConsulta.SelectedIndex = -1
+
+                Catch ex As Exception
+                    MessageBox.Show("Erro ao carregar avaliações: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+
+            End Using
+        End Using
+
+    End Sub
+
+
+End Class
