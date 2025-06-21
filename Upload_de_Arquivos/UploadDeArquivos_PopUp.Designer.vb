d54ff72af44c7552b68b844cc7132383197ec689
Luiz Palma <luizpalma16@gmail.com>
2025-06-21 13:00:57 -0300

    Form/PopUp de upload de arquivos
    TODO:
    - Cadastro no banco de dados
    - Navegação

---
+++ Upload_de_Arquivos/UploadDeArquivos_PopUp.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UploadDeArquivos_PopUp
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn_upload = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Pnl_drop = New System.Windows.Forms.Panel()
+        Me.Lb_arraste = New System.Windows.Forms.Label()
+        Me.Pnl_drop.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Btn_upload
+        '
+        Me.Btn_upload.Location = New System.Drawing.Point(697, 403)
+        Me.Btn_upload.Name = "Btn_upload"
+        Me.Btn_upload.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_upload.TabIndex = 0
+        Me.Btn_upload.Text = "UPLOAD"
+        Me.Btn_upload.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Pnl_drop
+        '
+        Me.Pnl_drop.AllowDrop = True
+        Me.Pnl_drop.Controls.Add(Me.Lb_arraste)
+        Me.Pnl_drop.Location = New System.Drawing.Point(103, 84)
+        Me.Pnl_drop.Name = "Pnl_drop"
+        Me.Pnl_drop.Size = New System.Drawing.Size(583, 308)
+        Me.Pnl_drop.TabIndex = 1
+        '
+        'Lb_arraste
+        '
+        Me.Lb_arraste.AutoSize = True
+        Me.Lb_arraste.Location = New System.Drawing.Point(195, 135)
+        Me.Lb_arraste.Name = "Lb_arraste"
+        Me.Lb_arraste.Size = New System.Drawing.Size(136, 13)
+        Me.Lb_arraste.TabIndex = 0
+        Me.Lb_arraste.Text = "Arraste os arquivos para cá"
+        '
+        'UploadDeArquivos_PopUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Btn_upload)
+        Me.Controls.Add(Me.Pnl_drop)
+        Me.Name = "UploadDeArquivos_PopUp"
+        Me.Text = "UploadDeArquivos_PopUp"
+        Me.Pnl_drop.ResumeLayout(False)
+        Me.Pnl_drop.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btn_upload As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Pnl_drop As Panel
+    Friend WithEvents Lb_arraste As Label
+End Class
---
+++ Upload_de_Arquivos/UploadDeArquivos_PopUp.vb
@@ -0,0 +1,104 @@
+﻿Imports System.Data.SQLite
+Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+
+Public Class UploadDeArquivos_PopUp
+    Private Sub Btn_upload_Click(sender As Object, e As EventArgs) Handles Btn_upload.Click
+
+        Dim ofd As New OpenFileDialog With {
+            .Filter = "Todos os arquivos (*.*)|*.*",
+            .Multiselect = True
+        }
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            For Each filePath As String In ofd.FileNames
+                If File.Exists(filePath) Then
+                    ProcessarArquivo(filePath)
+                Else
+                    MessageBox.Show("Arquivo não encontrado: " & filePath)
+                End If
+            Next
+        End If
+
+
+    End Sub
+
+#Region "Eventos de arrastar e soltar"
+
+    Private Sub UploadDeArquivos_PopUp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Pnl_drop.AllowDrop = True
+    End Sub
+
+
+    Private Sub Pnl_drop_DragEnter(sender As Object, e As DragEventArgs) Handles Pnl_drop.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+
+    Private Sub Pnl_drop_DragDrop(sender As Object, e As DragEventArgs) Handles Pnl_drop.DragDrop
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+        For Each filePath As String In files
+            If File.Exists(filePath) Then
+                ProcessarArquivo(filePath)
+            Else
+                MessageBox.Show("Arquivo não encontrado: " & filePath)
+            End If
+        Next
+    End Sub
+
+
+
+
+#End Region
+
+
+    Private Sub ProcessarArquivo(filePath As String)
+        Try
+            Dim fileName As String = Path.GetFileName(filePath)
+
+            Dim uploadDir As String
+            If Sessao.tipoUsuario = Sessao.ETipoUsuario.Professor Then
+                uploadDir = Application.StartupPath & "\uploads\professor"
+
+            ElseIf Sessao.tipoUsuario = Sessao.ETipoUsuario.Aluno Then
+                uploadDir = Application.StartupPath & "\uploads\professor"
+
+            Else
+                uploadDir = Application.StartupPath & "\uploads"
+                'MessageBox.Show("Tipo de usuário não reconhecido.")
+                'Return
+
+            End If
+
+
+            If Not Directory.Exists(uploadDir) Then Directory.CreateDirectory(uploadDir)
+
+            Dim destPath As String = Path.Combine(uploadDir, fileName)
+            File.Copy(filePath, destPath, True)
+
+            'SalvarArquivoNoBanco(fileName, destPath)
+            MessageBox.Show("Arquivo '" & fileName & "' enviado com sucesso!")
+        Catch ex As Exception
+            MessageBox.Show("Erro ao enviar: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+
+
+    Private Sub SalvarArquivoNoBanco(nomeArquivo As String, caminhoArquivo As String)
+        Dim connStr As String = "Data Source=seubanco.sqlite;Version=3;"
+        Using conn As New SQLiteConnection(connStr)
+            conn.Open()
+            Dim cmd As New SQLiteCommand("INSERT INTO arquivos (nome, caminho) VALUES (@nome, @caminho)", conn)
+            cmd.Parameters.AddWithValue("@nome", nomeArquivo)
+            cmd.Parameters.AddWithValue("@caminho", caminhoArquivo)
+            cmd.ExecuteNonQuery()
+        End Using
+    End Sub
+
+
+End Class
